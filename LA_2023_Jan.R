1e660eea8bf427794217881daa7e9de732597bc7
ttdangg <ttdang1445@gmail.com>
2025-05-03 17:58:31 -0700

    Add files via upload

--- LA_2023_Jan.R
+++ LA_2023_Jan.R
@@ -9,8 +9,49 @@ wrkdir <- getwd()
 wrkdir
 list.files()
 
-LA_2023_Jan <- read.csv("2023_01_Jan.csv", stringAsFactors == FALSE)
+LA_2023_Jan <- read.csv("2023_01_Jan.csv", stringsAsFactors = FALSE)
 LA_2023_Jan
 str(LA_2023_Jan)
 head(LA_2023_Jan)
 tail(LA_2023_Jan)
+summary(LA_2023_Jan)
+
+num_observation <- nrow(LA_2023_Jan)
+num_observation
+cat("There are", num_observation, "rows of observation.")
+
+num_variables <-ncol(LA_2023_Jan)
+num_variables
+cat("There are", num_variables,"variables.")
+
+num_observation <- nrow(LA_2023_Jan)
+num_observation
+cat("There are", num_observation, "rows of observation.")
+
+#Area
+cat(names(LA_2023_Jan), sep="\n")
+unique(LA_2023_Jan$AREA.NAME)
+length(unique((LA_2023_Jan$AREA.NAME)))
+table(LA_2023_Jan$AREA.NAME)
+AREA <- table(LA_2023_Jan$AREA.NAME)
+AREA
+
+#Crime 
+unique(LA_2023_Jan$Crm.Cd.Desc)
+length(unique(LA_2023_Jan$Crm.Cd.Desc))
+table(LA_2023_Jan$Crm.Cd.Desc)
+max <- which.max(table(LA_2023_Jan$Crm.Cd.Desc))
+max
+max_count = max()
+
+
+#Location
+unique(LA_2023_Jan$LOCATION)
+length(unique(LA_2023_Jan$LOCATION))
+
+
+#Premise
+unique(LA_2023_Jan$Premis.Desc)
+length(unique(LA_2023_Jan$Premis.Desc))
+
+
